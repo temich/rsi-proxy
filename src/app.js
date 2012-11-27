8045cff2a15547e7b4188bfef6624d239db3d735
a.gurtovoy <a.gurtovoy@it-online.ru>
2012-11-27 11:21:23 +0400

    Added logger

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ var domain = require('domain'),
 	redis = require('redis'),
 	connect = require('connect'),
 	rsi = require('rsi'),
-	config = require('./config.json');
+	config = require('./config.json'),
+	logger = require('tracer').dailyfile(config.tracer);
 
 var appd = domain.create(),
 	cache = redis.createClient(config.redis.port, config.redis.host);
 
 appd.on('error', function(err) {
-	// TODO: logger
-	console.error('Error:', err);
+	logger.error(err);
+	console.error(err);
 	appd.dispose();
 	cache.end();
 	process.exit(1);
@@ -23,19 +24,19 @@ cache.on('ready', function() {
 		cache.select(config.redis.db);
 	}
 
-	console.log('Redis connected to ' + config.redis.host + ':' + config.redis.port + (config.redis.db ? '[' + config.redis.db + ']' : ''));
+	logger.info('Redis connected to ' + config.redis.host + ':' + config.redis.port + (config.redis.db ? '[' + config.redis.db + ']' : ''));
 });
 
 appd.run(function() {
 
-	fs.mkdir(config.rsi.cache.root);
+	fs.existsSync(config.tracer.root) || fs.mkdir(config.rsi.cache.root);
+	fs.existsSync(config.tracer.root) || fs.mkdirSync(config.tracer.root);
 
 	connect()
-		.use(rsi.filter(cache, config.rsi))
 		.use(rsi.proxy(cache, config.rsi))
-		.listen(config.port);
+		.listen(process.env.PORT || config.port);
 
-	console.log('Server started on port ' + config.port);
+	logger.info('Server started ' + process.pid);
 });
 
 
